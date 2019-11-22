e0e0e2893195bb526e50c2d3dbe01002618f5634
Thomas Guyot <thomas.Evariste.guyot@gmail.com>
2019-11-22 21:41:30 +0100

    add genetic

--- tournoi_tictac/src/Tournoi/Algogenetique1.java
+++ tournoi_tictac/src/Tournoi/Algogenetique1.java
@@ -46,14 +46,17 @@ public class Algogenetique1 {
 		random = new Random();
 	}
 
-	int[][] work(int nbIte, double prob_mutation) {
+	int[][] work(int nbIte, int prob_mutation, int nb_max_mutation) {// prob_mutation
+																		// entre
+																		// 0 et
+																		// 100
 
 		Tournoi tournoi = new Tournoi();
 
 		for (int i = 0; i < nbIte - 1; i++) {
-			
-			System.out.println("debut de l'iteratin "+i);
-			
+
+			System.out.println("debut de l'iteratin " + i);
+
 			ArrayList<IA> Joueurs = new ArrayList<IA>(Joueurs_test);
 			for (int j = 0; j < 30; j++) {
 				Ia_scoring J = new Ia_scoring(IAs[j][0], IAs[j][1], IAs[j][2], IAs[j][3], IAs[j][4], IAs[j][5],
@@ -64,7 +67,7 @@ public class Algogenetique1 {
 			int[][] resu = tournoi.league(Joueurs);
 
 			int[][] IAs_int = choix_ia(resu);
-			IAs = reproduction(IAs_int);
+			IAs = reproduction(IAs_int, prob_mutation, nb_max_mutation);
 
 		}
 
@@ -104,27 +107,27 @@ public class Algogenetique1 {
 	private int[][] choix_ia(int[][] resu) {
 		int[][] IAs_int = new int[30][8];
 
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 10; i++) {
 			IAs_int[i] = new_IA_alea();
 		}
 
-		for (int i = 5; i < 20; i++) {
+		for (int i = 10; i < 25; i++) {
 			IAs_int[i] = IAs[random.nextInt(30)];
 		}
 
-		add_top10(IAs_int, resu);
+		add_top5(IAs_int, resu);
 
 		return IAs_int;
 	}
 
-	private void add_top10(int[][] iAs_int, int[][] resu) {
+	private void add_top5(int[][] iAs_int, int[][] resu) {
 		int compt = 0;
 
 		for (int[] joueur : resu) {
 			if (joueur[0] > nb_Joueurs_test - 1) {
-				iAs_int[20 + compt] = IAs[joueur[0] - (nb_Joueurs_test)];
+				iAs_int[25 + compt] = IAs[joueur[0] - (nb_Joueurs_test)];
 				compt++;
-				if (compt == 10) {
+				if (compt == 5) {
 					break;
 				}
 			}
@@ -132,9 +135,48 @@ public class Algogenetique1 {
 
 	}
 
-	private int[][] reproduction(int[][] iAs_int) {
+	private int[][] reproduction(int[][] IAs_int, int prob_mutation, int nb_max_mutation) {
+		int[][] IAs_ret = new int[30][8];
 
-		return iAs_int;
+		for (int i = 0; i < 2; i++) {
+			IAs_ret[i] = IAs_int[i + 20];
+		}
+
+		for (int i = 0; i < 14; i++) {
+			int ran1 = random.nextInt(30);
+			int ran2 = random.nextInt(30);
+			int ran3 = random.nextInt(8);
+			int ran4 = random.nextInt(100);
+			int ran5 = random.nextInt(100);
+			int[] IA1 = new int[8];
+			int[] IA2 = new int[8];
+			for (int j = 0; j < 8; j++) {
+				if (j < ran3) {
+					IA1[j] = IAs[ran1][j];
+					IA2[j] = IAs[ran2][j];
+				} else {
+					IA1[j] = IAs[ran2][j];
+					IA2[j] = IAs[ran1][j];
+				}
+			}
+			if (ran4 < prob_mutation) {
+				mutation(IA1, nb_max_mutation);
+			}
+			if (ran5 < prob_mutation) {
+				mutation(IA2, nb_max_mutation);
+			}
+			IAs_ret[2 * i + 2] = IA1;
+			IAs_ret[2 * i + 3] = IA2;
+		}
+
+		return IAs_ret;
+	}
+
+	private void mutation(int[] IA, int nb_max_mutation) {
+		for (int i = 0; i < random.nextInt(nb_max_mutation) + 1; i++) {
+			int ran = random.nextInt(8);
+			IA[ran] = random.nextInt(max[ran] + 1 - min[ran]) + min[ran];
+		}
 	}
 
 }
--- tournoi_tictac/src/Tournoi/exe.java
+++ tournoi_tictac/src/Tournoi/exe.java
@@ -11,24 +11,24 @@ public class exe {
 		// tournoi.one_match(J1,J2);
 		//
 		ArrayList<IA> Joueurs = new ArrayList<IA>();
-		for (int i = 0; i < 30; i++) {
+		for (int i = 0; i < 3; i++) {
 			Ia_simple J = new Ia_simple();
 			Joueurs.add(J);
 		}
 		// tournoi.league(Joueurs);
 		// Ia_scoring J2 = new Ia_scoring(20, 0, 10, 50, 25, -45, -35, -25);
 		// tournoi.one_match(J1, J2);
-		int[] min = {-50, -50, -50, 0, 0, -100, -100, -100};
-		int[] max = {50, 50, 50, 100, 100, 0, 0, 0};
+		int[] min = { -500, -500, -500, 0, 0, -1000, -1000, -1000 };
+		int[] max = { 500, 500, 500, 1000, 1000, 0, 0, 0 };
 		Algogenetique1 alg = new Algogenetique1(min, max, Joueurs);
 
-		int[][] top10 = alg.work(1000, 0);
-		
-		for(int[] j : top10){
+		int[][] top10 = alg.work(1500, 75, 4);
+
+		for (int[] j : top10) {
 			System.out.println();
 			System.out.println("les facteur de l'ia sont : ");
-			for(int fac:j){
-				System.out.print(" "+fac);
+			for (int fac : j) {
+				System.out.print(" " + fac);
 			}
 		}
 	}
